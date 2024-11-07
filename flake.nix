b546197ce5271472b55da1ca7a4ea76d39d9bc8f
Federico Serafini <federico@donq.io>
2024-11-07 10:30:13 +0100

    chore: testing pkgs-unstable import

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     # nix-darwin.url = "github:LnL7/nix-darwin";
     # nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
@@ -12,45 +13,50 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = inputs @ {
-    self,
-    flake-utils,
-    nixpkgs,
-    ...
-  }:
-    flake-utils.lib.eachSystem ["aarch64-darwin" "x86_64-darwin"] (
-      system: let
-        pkgs = import nixpkgs {inherit system;};
-      in {
-        darwinModules = {
-          default = {...}: {
-            environment.systemPackages = [pkgs.cowsay];
-            imports = [./shared/configuration.nix];
+  outputs =
+    inputs @ { self
+    , flake-utils
+    , nixpkgs
+    , nixpkgs-unstable
+    , ...
+    }:
+    flake-utils.lib.eachSystem [ "aarch64-darwin" "x86_64-darwin" ]
+      (
+        system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+          pkgs-unstable = import nixpkgs-unstable { inherit system; };
+        in
+        {
+          darwinModules = {
+            default = { ... }: {
+              environment.systemPackages = [ pkgs.cowsay ];
+              imports = [ ./shared/configuration.nix ];
+            };
           };
-        };
-        homeManagerModules = {
-          default = {...}: {
-            imports = [./shared/home.nix];
+          homeManagerModules = {
+            default = { ... }: {
+              imports = [ ./shared/home.nix ];
+            };
           };
-        };
 
-        packages.templater = pkgs.writeShellApplication {
-          name = "templater";
-          runtimeInputs = [pkgs.gnused];
-          text = ''
-            flake_directory=$(dirname "$4")
-            mkdir -p "$flake_directory"
-            sed -e "s/HOSTNAME/$1/g" -e "s/USERNAME/$2/g" -e "s/PLATFORM/$3/g" ${self}/template/flake.nix > "$4"
-          '';
-        };
+          packages.templater = pkgs.writeShellApplication {
+            name = "templater";
+            runtimeInputs = [ pkgs.gnused ];
+            text = ''
+              flake_directory=$(dirname "$4")
+              mkdir -p "$flake_directory"
+              sed -e "s/HOSTNAME/$1/g" -e "s/USERNAME/$2/g" -e "s/PLATFORM/$3/g" ${self}/template/flake.nix > "$4"
+            '';
+          };
 
-        # USAGE: nix run .#templater -- hostname username platform path/to/output/flake.nix
-        apps.templater = {
-          type = "app";
-          program = "${self.packages.${system}.templater}/bin/templater";
-        };
-      }
-    )
+          # USAGE: nix run .#templater -- hostname username platform path/to/output/flake.nix
+          apps.templater = {
+            type = "app";
+            program = "${self.packages.${system}.templater}/bin/templater";
+          };
+        }
+      )
     // {
       templates.default = {
         path = ./template;
