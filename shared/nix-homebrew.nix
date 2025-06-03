b320092a718a309ebb7204f5ac28d9442660efdd
Federico Serafini <federico@donq.io>
2025-06-03 11:33:14 +0200

    chore: commenta l'abilitazione di Rosetta nella configurazione di Homebrew

--- shared/nix-homebrew.nix
+++ shared/nix-homebrew.nix
@@ -4,7 +4,7 @@
     user = username;
     mutableTaps = false;
     autoMigrate = true;
-    enableRosetta = true;
+    #enableRosetta = true;
 
     taps = {
       "homebrew/homebrew-core" = inputs.homebrew-core;
